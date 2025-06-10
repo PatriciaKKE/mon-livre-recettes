4f2a2d22fa664f086e0e9b0365e1acffa75a650f
PatriciaKKE <kamleuemmerencia@gmail.com>
2025-06-10 12:04:12 +0200

    api

--- backend/livrerecettes_app/views.py
+++ backend/livrerecettes_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Recipe
+from .serializers import RecipeSerializer
 
-# Create your views here.
+class RecipeListCreate(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
